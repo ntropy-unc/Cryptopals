842d7f8faa884fb7255792b1e293ef0f74b73a18
boworkgo <bowork189@gmail.com>
2019-11-07 12:26:29 -0500

    finish 7

---
+++ set1_n7.py
@@ -0,0 +1,11 @@
+from base64 import b64decode
+from Crypto.Cipher import AES
+
+KEY = "YELLOW SUBMARINE"
+
+with open('7.txt') as f:
+    data = ''.join(map(lambda a: a.strip(), f.readlines()))
+    data = b64decode(data)
+    # print(data)
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    print(cipher.decrypt(data))
